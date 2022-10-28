80ebe1b859b56834538caa5b8c6a196eb207beea
TungLe0319 <tung.le0319@gmail.com>
2022-10-28 12:36:19 -0600

    tung

--- AllSpice/dbSetup.sql
+++ AllSpice/dbSetup.sql
@@ -38,12 +38,13 @@ CREATE TABLE
         name VARCHAR(255) NOT NULL,
         quantity VARCHAR(255) NOT NULL,
         recipeId INT NOT NULL,
-        FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+        FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
     ) default charset utf8 COMMENT '';
 
 CREATE TABLE
     IF NOT EXISTS favorites(
-        id VARCHAR(255) NOT NULL PRIMARY KEY ,
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
         updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
         accountId VARCHAR(255) NOT NULL,
